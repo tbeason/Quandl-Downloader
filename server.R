19b3ee5863da1ca1b65436389c801761136aed34
Tyler Beason <tbeas12@gmail.com>
2015-08-06 01:28:24 -0700

    all but the DL
    browser is having trouble pushing the tgz for download

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 ##server.R
 library(shiny)
 library(Quandl)
-#library(stringr)
+library(stringr)
 
 symbolList <- reactiveFileReader(100,NULL,'https://s3.amazonaws.com/quandl-static-content/Ticker+CSV%27s/WIKI_tickers.csv',read.csv,stringsAsFactors=FALSE,header=TRUE)
 
@@ -17,14 +17,36 @@ shinyServer(function(input, output,session) {
 #     vapply(X=symbolList()[,1], FUN=prettyCodes())
 #   })
   
-  #observe({updateSelectizeInput(session,'stocks',choices=symbolList()[,1],server=TRUE)})
+  observe({updateSelectizeInput(session,'stocks',choices=symbolList()[,2],server=TRUE)})
   
   output$downloadData <- downloadHandler(
     filename = function() { 
-      paste(input$filename, '.tar', sep='') 
+      paste(input$filename, '.tgz', sep='') 
     },
     content = function(file) {
-      write.csv(datasetInput(), file)
+      selected <- input$stocks
+      sList <- symbolList()
+      rowIndex <- unlist(sapply(selected,function(x){which(sList[,2]==x)}))
+      pullList <- sList[rowIndex,1]
+      pullFun <- function(pList){
+        startDate <- input$dateRange[[1]]
+        endDate <- input$dateRange[[2]]
+        freq <- input$frequency
+        
+        pullOne <- function(pCode){
+          fName <- str_replace(pCode,"/","-")
+          write.csv(Quandl(pCode,type="xts",start_date = startDate,end_date = endDate,collapse = freq),file=paste("temp/",fName,".csv",sep=""))
+        }
+        
+        sapply(pullList, pullOne)
+      }
+      
+      pullFun(pullList)
+      
+      tar(paste(input$filename,'.tgz',sep=""),list.files(), compression = 'gzip')
+#       if (file.exists(paste0(input$filename, ".tgz")))
+#         file.rename(paste0(input$filename, ".tgz"), input$filename)
     }
+
   )
 })
