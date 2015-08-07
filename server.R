3227d69f5c5a42c01020d39c34b03b58412f92da
Tyler Beason <tbeas12@gmail.com>
2015-08-06 17:22:13 -0700

    clean up

--- server.R
+++ server.R
@@ -3,19 +3,9 @@ library(shiny)
 library(Quandl)
 library(stringr)
 
-symbolList <- reactiveFileReader(100,NULL,'https://s3.amazonaws.com/quandl-static-content/Ticker+CSV%27s/WIKI_tickers.csv',read.csv,stringsAsFactors=FALSE,header=TRUE)
+symbolList <- reactiveFileReader(1000000,NULL,'https://s3.amazonaws.com/quandl-static-content/Ticker+CSV%27s/WIKI_tickers.csv',read.csv,stringsAsFactors=FALSE,header=TRUE)
 
 shinyServer(function(input, output,session) {
-#   prettyCodes <- function(aCode){
-#     str_sub(aCode,6,-1)
-#   }
-  
-#   searchHelperList <- reactive({
-#     
-#   })
-#   displayList <- reactive({
-#     vapply(X=symbolList()[,1], FUN=prettyCodes())
-#   })
   
   observe({updateSelectizeInput(session,'stocks',choices=symbolList()[,2],server=TRUE)})
   
